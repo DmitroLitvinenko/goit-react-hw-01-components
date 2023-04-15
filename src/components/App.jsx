52e4d0a01cc1ebcac5676d4faba4df2fac2cfe6b
Dmytro Litvinenko <dima.litvinenko1990@gmail.com>
2023-04-15 14:59:29 +0300

    create components Frienditem, FriendsList

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,15 @@ import user from '../user.json';
 import { Profile } from './Profile';
 import { StatisticsList } from './StatisticsList';
 import data from '../data.json';
+import friends from '../friends.json';
+import { FriendsList } from './FriendsList';
 
 export const App = () => {
   return (
     <div>
       <Profile user={user} />
       <StatisticsList items={data} title={'Upload stats'} />
+      <FriendsList friends={friends} />
     </div>
   );
 };
---
+++ src/components/FriendItem.jsx
@@ -0,0 +1,9 @@
+export const FriendItem = ({ friend: { avatar, name, isOnline } }) => {
+  return (
+    <>
+      <span>{isOnline ? 'Online' : 'Offline'}</span>
+      <img src={avatar} alt={name} width={48} />
+      <p>{name}</p>
+    </>
+  );
+};
